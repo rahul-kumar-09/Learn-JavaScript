07cded12a30beeacd1a5e9e556eea311404d63ac
Rahul kumar maury <rahu7827759@gmail.com>
2024-04-12 23:11:27 +0530

    Inroduction about JavaScript

---
+++ Day54 intorduction JavaScript/new.js
@@ -0,0 +1,4 @@
+
+console.log("Node Console ")
+console.log("Console Hello")
+console.log("Console Test")
---
+++ Day54 intorduction JavaScript/script.js
@@ -0,0 +1,23 @@
+alert("Hello World");
+
+console.log("Console Log")
+console.log("Console Hello")
+console.log("Console Test")
+
+var a = prompt("Enter your number ")
+console.log("Your Number is " + a)
+
+var isTrue = confirm("Are you sure to exit this page!")
+
+if(isTrue){
+    console.log("True value")
+} else{
+    console.log("False value")
+}
+
+
+// change title of page 
+document.title = "JavaScript Page"
+
+// change background Color || add inline css in html 
+document.body.style.backgroundColor = "purple"
