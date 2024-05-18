aa78fb5b2b5cffb88f8e9941ef2f0ae99fd64cf7
Rahul kumar maury <rahu7827759@gmail.com>
2024-05-18 16:11:21 +0530

    DOM 2

---
+++ Day67 DOM 2/script.js
@@ -0,0 +1,28 @@
+console.log("hello")
+
+// document.childNodes
+// document.body.childNodes[1]
+// document.body.childNodes[1].childNodes -> text, element
+// document.body.childNodes[1].childNodes.firstChild
+
+/*
+let count = document.body.childNodes[1]
+count.firstChild -> text
+coutn.lastChild -> text
+
+
+count.firstElementChild -> element
+count.lastElementChild
+
+count.firstElementChild.style.color = "red";
+count.firstElementChild.parentElement
+
+count.firstElementChild.children 
+count.firstElementChild.children[2] 
+
+document.body.childNodes[1].children[1].nextElementSibling
+<div class=​"box">​3​</div>​
+document.body.childNodes[1].children[1].previousElementSibling
+<div class=​"box">​1​</div>​
+
+*/
